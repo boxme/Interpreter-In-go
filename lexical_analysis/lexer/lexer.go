43d1a0fa08a5ca6da0c8eff3904948828dee4bbc
desmond <desmond@replaid.com>
2017-12-03 16:16:06 -0800

    Pass first test of lexer

--- lexical_analysis/lexer/lexer.go
+++ lexical_analysis/lexer/lexer.go
@@ -1,12 +1,61 @@
 package lexer
 
+import "../token"
+
 type Lexer struct {
-	input        string
-	position     int // current position in input (points to current char)
-	readPosition int
-	ch           byte // current char under examination
+	input           string
+	currentPosition int  // current position in input (points to current char)
+	nextPosition    int  // current reading position in input (after current char)
+	ch              byte // current char under examination
 }
 
 func New(input string) *Lexer {
-	return &Lexer{input: input}
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
+}
+
+func (lexer *Lexer) readChar() {
+	if lexer.nextPosition >= len(lexer.input) {
+		lexer.ch = 0
+	} else {
+		lexer.ch = lexer.input[lexer.nextPosition]
+	}
+
+	lexer.currentPosition = lexer.nextPosition
+	lexer.nextPosition += 1
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch lexer.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, lexer.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, lexer.ch)
+	case '(':
+		tok = newToken(token.LPAREN, lexer.ch)
+	case ')':
+		tok = newToken(token.RPAREN, lexer.ch)
+	case ',':
+		tok = newToken(token.COMMA, lexer.ch)
+	case '+':
+		tok = newToken(token.PLUS, lexer.ch)
+	case '{':
+		tok = newToken(token.LBRACE, lexer.ch)
+	case '}':
+		tok = newToken(token.RBRACE, lexer.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	lexer.readChar()
+
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
