b1faa9e876900ff13f6f9f1dbf119df8e54b2d75
desmond <desmond@replaid.com>
2017-12-03 18:20:18 -0800

    Expand lexer test to include keywords and integer

--- lexical_analysis/lexer/lexer.go
+++ lexical_analysis/lexer/lexer.go
@@ -29,6 +29,8 @@ func (lexer *Lexer) readChar() {
 func (lexer *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	lexer.skipWhitespace()
+
 	switch lexer.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, lexer.ch)
@@ -49,6 +51,20 @@ func (lexer *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(lexer.ch) {
+			tok.Literal = lexer.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}
+
+		if isDigit(lexer.ch) {
+			tok.Type = token.INT
+			tok.Literal = lexer.readNumber()
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, lexer.ch)
 	}
 
 	lexer.readChar()
@@ -56,6 +72,38 @@ func (lexer *Lexer) NextToken() token.Token {
 	return tok
 }
 
+func (lexer *Lexer) skipWhitespace() {
+	for lexer.ch == ' ' || lexer.ch == '\t' || lexer.ch == '\n' || lexer.ch == '\r' {
+		lexer.readChar()
+	}
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (lexer *Lexer) readIdentifier() string {
+	position := lexer.currentPosition
+	for isLetter(lexer.ch) {
+		lexer.readChar()
+	}
+
+	return lexer.input[position:lexer.currentPosition]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (lexer *Lexer) readNumber() string {
+	position := lexer.currentPosition
+	for isDigit(lexer.ch) {
+		lexer.readChar()
+	}
+
+	return lexer.input[position:lexer.currentPosition]
 }
