83be8e1f17a053b70383b0ef571cd1cee6f9184a
desmond <desmond@replaid.com>
2017-12-16 23:37:55 -0800

    Preparation for Pratt Parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,14 +1,23 @@
 package ast
 
-import "interpreter_using_go/token"
+import (
+	"bytes"
+	"interpreter_using_go/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
 	Node
 	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
 }
 
 // Implicitly implements Statement interface
@@ -20,13 +29,45 @@ type LetStatement struct {
 
 // These indicates what interface is LetStatement implementing
 func (ls *LetStatement) statementNode() {}
+
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
-type Expression interface {
-	Node
-	expressionNode()
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// Statement wrapper for expression
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
 }
 
 type ReturnStatement struct {
@@ -35,8 +76,23 @@ type ReturnStatement struct {
 }
 
 func (rs *ReturnStatement) statementNode() {}
+
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 // Implments Expression interface
@@ -46,8 +102,13 @@ type Identifier struct {
 }
 
 func (i *Identifier) expressionNode() {}
+
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 // Implements Node interface
@@ -61,4 +122,14 @@ func (p *Program) TokenLiteral() string {
 	}
 
 	return ""
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
