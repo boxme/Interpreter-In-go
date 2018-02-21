1eaf22dd5f5fb13a023e6925457e569ca1599cb8
desmond <desmond@replaid.com>
2018-02-20 18:57:59 -0800

    Expand on REPL to parse tokens into AST nodes

--- repl/repl.go
+++ repl/repl.go
@@ -1,10 +1,10 @@
 package repl
 
 import (
-	"interpreter_using_go/lexer"
-	"interpreter_using_go/token"
 	"bufio"
 	"fmt"
+	"interpreter_using_go/lexer"
+	"interpreter_using_go/parser"
 	"io"
 )
 
@@ -22,9 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
