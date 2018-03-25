4c9b0b9c9405660f545e12f962b90caa1e82840d
desmond <desmond@replaid.com>
2018-03-24 17:56:19 -0700

    Evaluate boolean

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"interpreter_using_go/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,11 +18,22 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	}
 
 	return nil
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
