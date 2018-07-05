adbc8687241d5994fb1d1fa4c4d4cd676309e61a
desmond <desmond@replaid.com>
2018-07-04 22:24:38 -0700

    Parse Strings to AST node

--- ast/ast.go
+++ ast/ast.go
@@ -299,6 +299,21 @@ func (ce *CallExpression) String() string {
 	return out.String()
 }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+
+func (sl *StringLiteral) TokenLiteral() string {
+	return sl.Token.Literal
+}
+
+func (sl *StringLiteral) String() string {
+	return sl.Token.Literal
+}
+
 // Implements Node interface
 type Program struct {
 	Statements []Statement
